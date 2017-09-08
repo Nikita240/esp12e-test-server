676b58ae9dd08c3487de72fa16e59b47e557cc87
Nikita Rushmanov <Rush3nik@gmail.com>
2017-09-07 23:16:14 -0700

    Add .onNotFound handler that calls SPIFFS

--- esp12e-test-server.ino
+++ esp12e-test-server.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <FS.h>
 
 const char* ssid = "240 Life";
 const char* password = "boostislife";
@@ -49,10 +50,51 @@ void setup(void){
 
 	server.on("/led", HTTP_POST, handleLed);
 
+
+	//called when the url is not defined here
+	//use it to load content from SPIFFS
+	server.onNotFound([](){
+		if(!handleFileRead(server.uri()))
+			server.send(404, "text/plain", "FileNotFound");
+	});
+
 	server.begin();
 	Serial.println("HTTP server started");
 }
 
 void loop(void){
 	server.handleClient();
+}
+
+String getContentType(String filename){
+	if(server.hasArg("download")) return "application/octet-stream";
+	else if(filename.endsWith(".htm")) return "text/html";
+	else if(filename.endsWith(".html")) return "text/html";
+	else if(filename.endsWith(".css")) return "text/css";
+	else if(filename.endsWith(".js")) return "application/javascript";
+	else if(filename.endsWith(".png")) return "image/png";
+	else if(filename.endsWith(".gif")) return "image/gif";
+	else if(filename.endsWith(".jpg")) return "image/jpeg";
+	else if(filename.endsWith(".ico")) return "image/x-icon";
+	else if(filename.endsWith(".xml")) return "text/xml";
+	else if(filename.endsWith(".pdf")) return "application/x-pdf";
+	else if(filename.endsWith(".zip")) return "application/x-zip";
+	else if(filename.endsWith(".gz")) return "application/x-gzip";
+	return "text/plain";
+}
+
+bool handleFileRead(String path){
+	DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+	if(path.endsWith("/")) path += "index.htm";
+	String contentType = getContentType(path);
+	String pathWithGz = path + ".gz";
+	if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+		if(SPIFFS.exists(pathWithGz))
+			path += ".gz";
+		File file = SPIFFS.open(path, "r");
+		size_t sent = server.streamFile(file, contentType);
+		file.close();
+		return true;
+	}
+	return false;
 }
